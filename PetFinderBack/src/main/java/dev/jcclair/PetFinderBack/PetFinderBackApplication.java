fc12460a1265123ac232df51b96d77fab1bf7fa5
Jason CLair <jasonclairsoftware@gmail.com>
2025-10-11 11:12:49 -0700

    Added the base front end and backend to the repository

---
+++ PetFinderBack/src/main/java/dev/jcclair/PetFinderBack/PetFinderBackApplication.java
@@ -0,0 +1,13 @@
+package dev.jcclair.PetFinderBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFinderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFinderBackApplication.class, args);
+	}
+
+}
---
+++ PetFinderBack/src/test/java/dev/jcclair/PetFinderBack/PetFinderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jcclair.PetFinderBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFinderBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
