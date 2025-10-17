366cff8758b5e24318e67ce898460872d544d497
Jason CLair <jasonclairsoftware@gmail.com>
2025-10-16 21:19:25 -0700

    Added routes for home and login and set up basic login form

--- PetFinderFront/src/app/app.routes.ts
+++ PetFinderFront/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:Home},
+    { path: 'login', component:Login }
+];
